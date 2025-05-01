b4c6d0054616f57c63a3104ca3859526bf421477
ntokozo-SA <Ntokozobuthelezi205@gmail.com>
2025-05-01 21:43:33 +0200

    final commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function generatePassword(length, includeUppercase, includeLowercase, includeNumbers, includeSymbols) {
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberChars = '0123456789';
+    const symbolChars = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+    let charSet = '';
+    if (includeUppercase) charSet += uppercaseChars;
+    if (includeLowercase) charSet += lowercaseChars;
+    if (includeNumbers) charSet += numberChars;
+    if (includeSymbols) charSet += symbolChars;
+
+    if (charSet.length === 0) {
+        throw new Error('At least one character set must be selected.');
+    }
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charSet.length);
+        password += charSet[randomIndex];
+    }
+
+    return password;
+}
+
+const passwordLength = 12;
+const includeUppercase = true;
+const includeLowercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+console.log(generatePassword(passwordLength, includeUppercase, includeLowercase, includeNumbers, includeSymbols));
